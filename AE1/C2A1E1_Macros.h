78f677ca1a657df7349e91ffe050006341808fb6
BRICE AUGUSTYN <35545541+briceaugustyn@users.noreply.github.com>
2022-01-20 20:54:42 -0800

    Create C2A1E1_Macros.h

---
+++ AE1/C2A1E1_Macros.h
@@ -0,0 +1,20 @@
+//Brice Augustyn (U07944417) baugustyn1000@gmail.com
+//C/C++ Programming II 
+//Section 160706 Ray Mitchell
+//1/18/22
+//C2A1E1_Macros.h
+//used GDB https://www.onlinegdb.com/#
+// This file contains header file including an "include guard" and three macros definitions
+
+
+#ifndef C2A1E1_MACROS_H
+#define C2A1E1_MACROS_H
+
+
+#define Product(a,b) ((a)*(b))
+#define Negate(a) (-(a))
+#define Elements(a) (sizeof(a)/sizeof(a[0]))
+
+#endif
+
+
